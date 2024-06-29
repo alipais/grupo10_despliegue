6088bddaaa009dbf2290e51d0aaa4a446ad8383e
alipais <aliciadri2011@gmail.com>
2024-06-29 13:34:20 -0300

    modifico  usuario paw

--- componentes/config_db.py
+++ componentes/config_db.py
@@ -1,10 +1,19 @@
 import mysql.connector
 
 config_dev = {
+    # configuración en desarrollo (local)
     "user": 'root',
     'password': '',
     'host': '127.0.0.1',
     'database': 'favorite_cake'
+}
+
+config_prod = {
+    # configuración en producción (despliegue)
+    "user": 'liciaG',
+    'password': 'grupo102024',
+    'host': 'AliciaG.mysql.pythonanywhere-services.com',
+    'database': 'AliciaG$favorite_cake'
 }
 
 conexion = mysql.connector.connect(**config_dev)
